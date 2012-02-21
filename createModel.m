e7fb0673bea078df100472b95e1614b4ec6938d6
Cliff Engle <cliffengle@gmail.com>
2012-02-21 14:47:37 -0800

    Fixed bugs and added check for uniques

--- createModel.m
+++ createModel.m
@@ -1,8 +1,7 @@
 
 load('smap.mat', 'smap');
 dictLen = length(smap);
-readLength = 60000000; % each item is 4 bytes. read multiple of 3 each time since we have 3 tokens
-
+numTokens = 20000000; % each item is 4 bytes. read multiple of 3 each time since we have 3 tokens
 X = []; % NxM matrix. N is num terms, M is num docs.
 y = []; % Nx1 matrix. Contains ratings
 
@@ -10,45 +9,62 @@ y = []; % Nx1 matrix. Contains ratings
 ratingIndex = strmatch('<rating>', smap, 'exact');
 reviewTextIndex = strmatch('<review_text>', smap, 'exact');
 endReviewTextIndex = strmatch('</review_text>', smap, 'exact');
-nextTokenIsRating = false;
-insideReviewText = false;
+endReviewIndex = strmatch('</review>', smap, 'exact');
 
-out = [];
+seen = containers.Map('KeyType', 'char', 'ValueType', 'logical');
 
 f = fopen('tokens.bin');
 partition = 1;
-c = 0;
-times = []
-%while ~feof(f)
-for j = 1:1
-  buf = fread(f, readLength, 'int32');
-  buf = buf(3:3:end);
-  reviewStarts = find(buf == reviewTextIndex);
-  reviewEnds = find(buf == endReviewTextIndex);
+Xtot = [];
+ytot = [];
+while ~feof(f)
+%for j = 1:1
   
-  if length(reviewStarts) > length(reviewEnds) 
-    reviewStarts = reviewStarts(1:end-1);
-  end
+  buf = fread(f, numTokens*3, 'int32');
+  buf = buf(3:3:end);
+  reviewTextEnds = find(buf == endReviewTextIndex);
+  reviewEnds = find(buf == endReviewIndex);
+  buf = buf(1:reviewEnds(end)); % stop at last whole review
+  fseek(f, -(numTokens - reviewEnds(end))*4*3,'cof'); % seek to end of last whole review
+  reviewTextStarts = find(buf == reviewTextIndex);
   ratingStarts = find(buf == ratingIndex);
-  y = cell2mat(smap(buf(ratingStarts + 1))) - '0'; % Get all ratings and convert them into numerical vector (1-5)
-  numReviews = length(reviewStarts)
+
+  uniques = logical([]); % indexes of unique reviews
+
+  y = cell2mat(smap(buf(ratingStarts + 1))) - '0'; % Get all ratings and convert them into numerical vector with values 1-5
+  numReviews = length(reviewTextStarts);
   tic;
-  X = sparse(1 + dictLen, numReviews)
+  X = sparse(1 + dictLen, numReviews);
   for i = 1:numReviews
-    text = buf(reviewStarts(i) + 1:reviewEnds(i) - 1);
+    text = buf(reviewTextStarts(i) + 1:reviewTextEnds(i) - 1);
+    key = mat2str(text(1:min(10,end)));
+    if isKey(seen, key)
+      continue
+    else
+      seen(key) = true;
+      uniques = [uniques; i];
+    end
     review = sparse(text, 1, 1, dictLen, 1);
     X(:,i) = [1; review];
-    if mod(i, 1000) == 0
-      display('done 1000: ')
+    if mod(i, 10000) == 0
+      display('done 10000: ')
       toc
       tic;
     end
   end
-  save(strcat('res',int2str(partition)), 'X', 'y')
+  toc;
+  display('Num uniques: ')
+  display(length(uniques))
+  Xtot = [Xtot, X(:,uniques)];
+  ytot = [ytot; y(uniques)];
   partition = partition + 1
 end
 
 fclose(f)
+
+%  save(strcat('res',int2str(partition)), 'X', 'y')
+
+% clearvars -except X y
 
 % B = (X * X' + eye(length(X)))\(X*y)
 % score = X'*B
