5b5c9875065d198278162cd813c8e4bcd127b898
Cliff Engle <cliffengle@gmail.com>
2012-02-25 00:40:00 -0800

    Added flops, lift

--- crossValidate.m
+++ crossValidate.m
@@ -4,12 +4,15 @@ numWords = 10000;
 numReviews = length(Xtot(1,:));
 
 iterations = 1
-c = 1; % regularization constant
+lambda = 1; % regularization constant
 rng(13);
 ri = randperm(numReviews);
 Xtot = Xtot(:,ri); % shuffle reviews
 ytot = ytot(ri); % shuffle reviews
 aucs = []
+lifts = []
+times = []
+flops = []
 for i = 1:iterations
   % separate data into training and validation
   validationStart = numReviews*(i-1)/10 + 1;
@@ -20,10 +23,24 @@ for i = 1:iterations
   validationY = ytot(validationStart:validationEnd);
   
   % get B from regression
-  r = speye(numWords)*c;
+  r = speye(numWords)*lambda;
+  tic;
   B = (trainingX * trainingX' + r)\(trainingX*trainingY);
+  times(i) = toc
+  
+  % calculate flops
+  nza = double(trainingX ~= 0);
+  nzb = double(trainingX' ~= 0);
+  ftmp = nza*nzb;
+  ftmp = 2*ftmp - (ftmp ~= 0);
+  f = sum(sum(ftmp));
+  flops(i) = f;
+
   scores = validationX'*B;
   [x,y,t,AUC] = perfcurve((validationY > 3),scores,true);
   aucs = [aucs, AUC];
+  [l, index] = min(abs(x-.01));
+  lifts = [lifts, y(index)/.01];
   display(i)
 end
+perf = flops ./ times;
